52d0278215966f9e8c54bea5f8389ba11d156033
Jeff Urmston <jeff.urmston@gmail.com>
2021-03-26 15:35:31 -0700

    Added rollup js for build support

---
+++ README.md
@@ -0,0 +1,2 @@
+Article on build process with rollup.js:
+https://hinammehra.medium.com/build-a-private-react-component-library-cra-rollup-material-ui-github-package-registry-1e14da93e790
---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import del from 'rollup-plugin-delete';
+import pkg from './package.json';
+
+export default {
+    input: pkg.source,
+    output: [
+        { file: pkg.main, format: 'cjs' },
+        { file: pkg.module, format: 'esm' }
+    ],
+    plugins: [
+        nodeResolve(),
+        external(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        del({ targets: ['dist/*'] }),
+    ],
+    external: Object.keys(pkg.peerDependencies || {}),
+};
