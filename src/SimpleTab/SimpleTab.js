89d258a51f18442606f8b7526d9c165dc1dae5e2
Jeff Urmston <jeff.urmston@gmail.com>
2021-09-07 15:38:18 -0700

    fix: removed button ripple on SimpleTab.

--- src/SimpleTab/SimpleTab.js
+++ src/SimpleTab/SimpleTab.js
@@ -1,16 +1,17 @@
-import { styled } from '@material-ui/styles'
+import { withStyles } from '@material-ui/styles'
 import Tab from '@material-ui/core/Tab'
 
 
-export const SimpleTab = styled(Tab)(({ theme }) => ({
-  textTransform: 'none',
-  alignItems: 'flex-start',
-  padding: 4,
-  minWidth: 0,
-  marginRight: theme.spacing(2),
-  ...theme.typography.subtitle2,
-
-  '&.MuiTab-labelIcon': {
+export const SimpleTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    alignItems: 'flex-start',
+    padding: 4,
+    minWidth: 0,
+    marginRight: theme.spacing(2),
+    ...theme.typography.subtitle2,
+  },
+  labelIcon: {
     flexDirection: 'row',
     alignItems: 'center',
     minHeight: 0,
@@ -19,4 +20,4 @@ export const SimpleTab = styled(Tab)(({ theme }) => ({
       marginBottom: 0,
     }
   },
-}))
+}))((props) => <Tab disableRipple {...props} />)
